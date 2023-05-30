e3f76f1383db0f5ad817ac9875ac75e8d4adf27c
Robert Košir <robikoser@gmail.com>
2023-05-30 14:28:17 +0200

    Add file upload on step 3 - crash

--- src/app/shared/components/ui/car-condition/car-condition.module.ts
+++ src/app/shared/components/ui/car-condition/car-condition.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CarConditionComponent } from './car-condition.component';
 import { VisibleDamageSelectorModule } from "../visible-damage-selector/visible-damage-selector.module";
 import { PointOfInitialImpactModule } from "../point-of-initial-impact/point-of-initial-impact.module";
+import { FileUploadModule } from "../../file-upload/file-upload.module";
 
 
 
@@ -13,10 +14,11 @@ import { PointOfInitialImpactModule } from "../point-of-initial-impact/point-of-
   exports: [
     CarConditionComponent
   ],
-  imports: [
-    CommonModule,
-    VisibleDamageSelectorModule,
-    PointOfInitialImpactModule
-  ]
+    imports: [
+        CommonModule,
+        VisibleDamageSelectorModule,
+        PointOfInitialImpactModule,
+        FileUploadModule
+    ]
 })
 export class CarConditionModule { }
