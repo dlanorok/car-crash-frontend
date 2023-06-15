a211991cea9b4946ceb55d79f3bec2c4de840d99
Robert Košir <robikoser@gmail.com>
2023-06-15 12:49:03 +0200

    Add proxy for ws

--- src/app/shared/services/web-socket.service.ts
+++ src/app/shared/services/web-socket.service.ts
@@ -42,7 +42,7 @@ export class WebSocketService implements OnDestroy {
 
   connect(): void {
     this.socket$ = webSocket<any>({
-      url: `wss://carcrashassist.snapp.email/ws/updates/${localStorage.getItem(StorageItem.sessionId)}/`,
+      url: `ws://localhost:4200/ws/updates/${localStorage.getItem(StorageItem.sessionId)}/`,
     });
 
     if (this.connected) {
