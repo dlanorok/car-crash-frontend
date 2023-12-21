797098392583bbd5a138e5be32d700d5ed71c180
matjazskulj <matjazs88@gmail.com>
2023-12-21 15:23:49 +0100

    prod config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://crash.aksi.si/api/v1',
+  webSocketUrl: 'wss://crash.aksi.si',
+  regulaUrl: 'https://regula.aksi.si',
+  googleApiKey: "AIzaSyCayvMRKELC7xnbGlrEurHGJPuRtUm1HOg",
+  googleApiUrl: "https://maps.googleapis.com/maps/api"
+};
