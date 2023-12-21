eec752751fa917285502e2582e252fb3cb471318
matjazskulj <matjazs88@gmail.com>
2023-12-21 15:45:47 +0100

    domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://crash.aksi.si/api/v1',
-  webSocketUrl: 'wss://crash.aksi.si',
-  regulaUrl: 'https://regula.aksi.si',
+  apiUrl: 'https://app.aksi.ai/api/v1',
+  webSocketUrl: 'wss://app.aksi.ai',
+  regulaUrl: 'https://regula.aksi.ai',
   googleApiKey: "AIzaSyCayvMRKELC7xnbGlrEurHGJPuRtUm1HOg",
   googleApiUrl: "https://maps.googleapis.com/maps/api"
 };
