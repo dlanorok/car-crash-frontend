e4d95d7a147fa58ec2c50d0c1207761d94121ab1
matjazskulj <matjazs88@gmail.com>
2024-01-04 12:42:29 +0100

    new map keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiUrl: 'https://app.aksi.ai/api/v1',
   webSocketUrl: 'wss://app.aksi.ai',
   regulaUrl: 'https://regula.aksi.ai',
-  googleApiKey: "AIzaSyCayvMRKELC7xnbGlrEurHGJPuRtUm1HOg",
+  googleApiKey: "AIzaSyBDH46o5WoP0nxR1iysDEEcsYruTHk_PrU",
   googleApiUrl: "https://maps.googleapis.com/maps/api"
 };
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiUrl: 'https://carcrashassist.snapp.email/api/v1',
   webSocketUrl: 'wss://carcrashassist.snapp.email',
   regulaUrl: 'https://regula.snapp.email',
-  googleApiKey: "AIzaSyCayvMRKELC7xnbGlrEurHGJPuRtUm1HOg",
+  googleApiKey: "AIzaSyCl54fo-PjW36JJh2-_v2_53YaPs-Tmxhs",
   googleApiUrl: "https://maps.googleapis.com/maps/api"
 };
